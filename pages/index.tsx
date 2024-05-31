4fe99a0fe306844de4758376e8bdebd0f6bbd904
Anang Hariyanto N <sokoanang@gmail.com>
2024-05-31 20:16:06 +0700

    blank commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,5 @@
-import type { NextPage } from "next";
-import { useEffect } from "react";
-import AOS from "aos";
-import Navbar from "../components/organisms/Navbar";
-import MainBanner from "../components/organisms/MainBanner";
-import TransactionStep from "../components/organisms/TransactionStep";
-import FeaturedGame from "../components/organisms/FeaturedGame";
-import Reached from "../components/organisms/Reached";
-import Story from "../components/organisms/Story";
-import Footer from "../components/organisms/Footer";
-import Head from "next/head";
-
-const Home: NextPage = () => {
-  useEffect(() => {
-    AOS.init();
-  }, []);
-  return (
-    <>
-      <Head>
-        <title>StoreGG - Get A New Experience in Gaming</title>
-        <meta name="description" content="Kami menyediakan jutaan cara untuk membantu
-players menjadi pemenang sejati"/>
-        <meta property="og:title" content="StoreGG - Gae a New Experience in Gaming"/>
-        <meta property="og:description" content="Kami menyediakan jutaan cara untuk membantu
-players menjadi pemenang sejati" />
-        
-      </Head>
-      <Navbar />
-      <MainBanner />
-      <TransactionStep />
-      <FeaturedGame />
-      <Reached />
-      <Story />
-      <Footer />
-    </>
-  );
+const Home = () => {
+  return <div>Welcome to home Page</div>;
 };
 
 export default Home;
